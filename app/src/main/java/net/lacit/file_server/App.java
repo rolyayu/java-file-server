a669adfbb79830cc783520cf3d9570c505fc894e
Anton Rudkosvky <ranton497@gmail.com>
2023-11-17 12:00:27 +0300

    refactor: add file downloader instance to upload service constructor

--- app/src/main/java/net/lacit/file_server/App.java
+++ app/src/main/java/net/lacit/file_server/App.java
@@ -6,6 +6,7 @@ import net.lacit.file_server.server.upload.UploadEndpoint;
 import net.lacit.file_server.server.upload.UploadService;
 import net.lacit.file_server.server.upload.UploadServiceImpl;
 import net.lacit.file_server.server.upload.UrlValidator;
+import net.lacit.file_server.server.upload.FileDownloader;
 import net.lacit.file_server.utils.FilePathUtils;
 import net.lacit.file_server.utils.PropertyReader;
 
@@ -27,8 +28,9 @@ public class App {
 
         Server server = new Server(port);
         UrlValidator urlValidator = new UrlValidator();
+        FileDownloader fileDownloader = new FileDownloader();
         FilePathUtils filePathUtils = new FilePathUtils(reader);
-        UploadService uploadService = new UploadServiceImpl(filePathUtils);
+        UploadService uploadService = new UploadServiceImpl(filePathUtils, fileDownloader);
         Endpoint uploadEndpoint = new UploadEndpoint(uploadService, urlValidator);
 
         server.addEndpoint(uploadEndpoint);
